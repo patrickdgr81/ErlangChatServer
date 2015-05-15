23c195abf18b80e89fd1afefcdbdbb4c3368ecb7
patrickdgr81 <patrickdgr81@gmail.com>
2015-05-15 10:56:01 -0700

    added tutorial and debugged chatServer

--- src/chatServer.erl
+++ src/chatServer.erl
@@ -6,26 +6,33 @@
 -record(server_state, {activePids,
                 history}).
 
+start_server(M) -> ok.
+
 %%% Server functions
 init([]) -> 
 	%% IMPORTANT: Start the empd daemon!
     os:cmd("epmd -daemon"),
     %% Register yourself as chatServer
     net_kernel:start([chatServer, shortnames]),    
-    {ok, #server_state{activePids = [], history = []}.
+    {ok, #server_state{activePids = [], history = []}}.
+
+code_change(_OldVsn, State, _Extra) ->
+	%% No change planned. The function is there for the behaviour,
+	%% but will not be used. Only a version on the next
+	{ok, State}.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%% SYNCRHONOUS MESSAGES %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%Getting the history of the server
 handle_call(getHistory, _From, S) ->
-	{reply, S#server_state.history, S}.
+	{reply, S#server_state.history, S};
 
 %%A new client requested to join
 handle_call(requestJoin, {Pid, _Tag}, S) ->
 	%%Monitor the process in case it crashes. 
 	%%If its gone, we remove it from the chatroom
-	{reply, ok, S#server_state{activePids = lists:append(S#server_state.history, [Pid])}}.
+	{reply, ok, S#server_state{activePids = lists:append(S#server_state.activePids, [Pid])}}.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%% END SYNCRHONOUS MESSAGES %%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -34,7 +41,7 @@ handle_call(requestJoin, {Pid, _Tag}, S) ->
 handle_cast({message, Message}, S) -> 
 	io:format("!~p Got Message: ~p~n", [now(), Message]),
 	tellEveryone(S#server_state.activePids, Message),
-	{noreply, S#server_state{activePids = lists:append(S#server_state.history, [Pid])}}.
+	{noreply, S#server_state{activePids = lists:append(S#server_state.history, [Message])}}.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%% END ASYNCRHONOUS MESSAGES %%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
