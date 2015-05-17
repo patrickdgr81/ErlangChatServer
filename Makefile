326fa5a83fcf3f75a08850453a686570d1716266
patrickdgr81 <patrickdgr81@gmail.com>
2015-05-16 22:16:05 -0700

    fixed Makefile bug and added monitoring of processes

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ all: compile
 
 compile:
 	erlc -o ebin/ src/*.erl
-	#clang++ -undefined dynamic_lookup -dynamiclib -std=c++11 -stdlib=libc++ -Wall -fPIC -O3 -I /opt/local/lib/erlang/erts-5.10.4/include -shared yahtzee_chooser.cpp -o yahtzee_chooser.so
+	
 clean:
 	rm *.beam
 	rm *.so
--- src/chatServer.erl
+++ src/chatServer.erl
@@ -6,19 +6,20 @@
 -record(server_state, {activePids,
                 history}).
 
-start_server(M) -> ok.
-
-%%% Server functions
-init([]) -> 
+start_server(M) -> 
 	%% IMPORTANT: Start the empd daemon!
     os:cmd("epmd -daemon"),
     %% Register yourself as chatServer
-    net_kernel:start([chatServer, shortnames]),    
+    net_kernel:start([chatServer, shortnames]),
+    gen_server:start({local, ?MODULE}, ?MODULE, {}, []);.
+
+%%% Server functions
+init(_Args) ->  
     {ok, #server_state{activePids = [], history = []}}.
 
 code_change(_OldVsn, State, _Extra) ->
 	%% No change planned. The function is there for the behaviour,
-	%% but will not be used. Only a version on the next
+	%% but will not be used.
 	{ok, State}.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -31,7 +32,8 @@ handle_call(getHistory, _From, S) ->
 %%A new client requested to join
 handle_call(requestJoin, {Pid, _Tag}, S) ->
 	%%Monitor the process in case it crashes. 
-	%%If its gone, we remove it from the chatroom
+	%%If its gone, we remove it from the chatroom later
+	erlang:monitor(process, Pid),
 	{reply, ok, S#server_state{activePids = lists:append(S#server_state.activePids, [Pid])}}.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%% END SYNCRHONOUS MESSAGES %%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -42,6 +44,15 @@ handle_cast({message, Message}, S) ->
 	io:format("!~p Got Message: ~p~n", [now(), Message]),
 	tellEveryone(S#server_state.activePids, Message),
 	{noreply, S#server_state{activePids = lists:append(S#server_state.history, [Message])}}.
+
+%%Process exited or died on its own, we remove it from the list of active pids
+handle_info({'DOWN', MonitorRef, Type, Pid, Reason}, S) ->
+	io:format("!~p Pid that died: ~p~n", [now(), Pid]),
+	{noreply, S#server_state{activePids = lists:delete(Pid, S#server_state.activePids)}};
+
+handle_info(Message,S) ->
+	io:format("!~p Got Unexpected Message: ~p~n", [now(), Message]),
+	{noreply, S}.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%% END ASYNCRHONOUS MESSAGES %%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
